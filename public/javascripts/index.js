4148d1c22477e9ab1e72fec6170c1454d457ef6a
unknown <administrator@jl2012.kl.kdo.int>
2014-10-15 06:36:04 +0200

    14.10.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,26 +9,30 @@ $(document).ready(function () {
   $('#nav_print').click( function(){
     $.getJSON( "/ajax/", {'cmd' : 'vlans' }, function( data ) {
       if (data.err) {
-        //alert(data.err)
-        
         $.Notify({
             style: {background: 'red', color: 'white'},
             shadow: true,
-    	      caption: "ERROR",
-            content: "<br><br>Bitte einloggen!",
+    	      caption: "Error",
+            content: "Bitte einloggen!",
             timeout: 10000 // 10 seconds
         });
-        
         return
       }
       var items = [];
       $.each( data.rows, function( key, val ) {
-        items.push( "<li id='" + key + "'>" + val.vlan_name + "</li>" );
+        items.push( "<tr><td id='" + key + "'>" + key + "</td><td>" + val.vlan_name + "</td></tr>" );
       })
-      $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-      }).appendTo( "body" );
+
+      $( "<table />", {
+        "class": "table table-fixed-header table-hover striped bordered hovered",
+        html: "<thead><tr><th>VLans</th><th>Name</th></tr></thead><tbody>" + items.join( "" ) + "</tbody>"
+      }).appendTo( "#vlans" );
+      $('#vlans table tr:last td').each( function(n,ui){
+        console.log( n + $(ui).html() )
+        console.log( $('#vlans table th:nth-child('+(n+1)+')').html() )
+        //$('#vlans table th:nth-child('+(n+1)+')').width( $(ui).width() )
+        $(ui).width( $('#vlans table th:nth-child('+(n+1)+')').width() )
+      } )
     })
     .done(function() {
       console.log( "second success" );
@@ -39,7 +43,7 @@ $(document).ready(function () {
     .always(function() {
       console.log( "complete" );
     });
- 
+  if ($('#menutoggle').is(":visible") ) $('#topmenu').toggle()
 
   })
 
