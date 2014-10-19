903a381a1b1ca4f098618c824768ca533e9fa6ed
unknown <Administrator@gandalf.SHIRE.local>
2014-10-19 22:26:06 +0200

    NetIO

--- routes/netio.js
+++ routes/netio.js
@@ -4,13 +4,29 @@ var router = express.Router();
 //var UserLogin = require('./includes/UserLogin')
 //var AppConfig= require("../AppConfig")
 
+
+
 router.get('/', function (req, res) {
+
   //UserLogin.initUser(req);
   //res.render('login', { AppConfig: AppConfig, user: req.session.user });
   res.render('netio/netio');
 
 });
 
+router.get("/DeviceList", function(req,res){
+  var fields
+  rows = new Array()
+  rows.push({
+      hostname : "pio01",
+      ip : "192.168.0.51"  
+    })
+  rows.push({
+      hostname : "pio00",
+      ip : "192.168.0.91"  
+    })
+  res.send({ err: "", rows: rows, fields: fields })
+})
 
 
 module.exports = router;
